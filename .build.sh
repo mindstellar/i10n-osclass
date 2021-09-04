50e3acd2ab86847643f4cf1a6686c8103bd09b9d
navjottomer <navjottomer@gmail.com>
2021-09-04 17:57:09 +0530

    Updated build script

--- .build.sh
+++ .build.sh
@@ -5,70 +5,75 @@
 ## Description
 ## Basic script for generating Osclass translations template
 ##
+## Usage:
+## ./build.sh
 ##
-
-# Delete any previous tmp directory
-if [ -d ./tmp ]; then
-    ### An existing directory exists delete it ###
-    echo 'removing existing tmp directory'
-    rm -r ./tmp
+# Delete any previous tmp directory if exists and print message
+if [ -d "tmp" ]; then
+  rm -rf tmp
+  echo "Deleted previous tmp directory"
 fi
+# save current directory to variable ROOT_DIR
+ROOT_DIR=$(pwd)
 ###  Make directory ###
 mkdir tmp
-mkdir tmp/osclass
-
-# Download latest Osclass release from repository
-echo 'Downloading latest osclass theme'
-(
-    cd tmp && curl -s https://api.github.com/repos/mindstellar/Osclass/releases/latest |
-        grep 'browser_download_url' |
-        head -1 |
-        cut -d '"' -f 4 |
-        wget -qi - && unzip -qq osclass*.zip -d .
-)
-
+# clone the git@github.com:mindstellar/osclass repo and change directory to it
+# get the current branch name
+branch=$(git rev-parse --abbrev-ref HEAD)
+# change directory to tmp
+cd tmp || exit
+# clone git@github.com:mindstellar/osclass and checkout same branch name in tmp directory
+git clone --branch "$branch" git@github.com:mindstellar/Osclass.git
+cd Osclass || exit
 #Create Fresh gettext pot file for core
 # shellcheck disable=SC2038
-find tmp/osclass/oc-admin/ tmp/osclass/oc-includes/osclass/ -type f -name '*.php' |
-    xargs xgettext \
-        --keyword=__ -k=_e -k=_n:1,2 \
-        --language=PHP \
-        --output-dir=src/templates \
-        --output=core.pot \
-        --from-code=utf-8 \
-        --msgid-bugs-address=translations@mindstellar.com
-
+find ./oc-admin/ ./oc-includes/osclass/ -type f -name '*.php' |
+  xargs xgettext \
+    --keyword=__ -k=_e -k=_n:1,2 \
+    --language=PHP \
+    --output-dir="$ROOT_DIR"/src/templates/ \
+    --output=core.pot \
+    --from-code=utf-8 \
+    --msgid-bugs-address=translations@mindstellar.com
 echo 'core.pot is created in /src/templates directory'
-
 #Create Fresh gettext pot file for messages
 # shellcheck disable=SC2038
-find tmp/osclass/oc-admin/ tmp/osclass/oc-includes/osclass/ -type f -name '*.php' |
-    xargs xgettext \
-        --keyword=_m -k=_mn:1,2 \
-        --language=PHP \
-        --output-dir=src/templates \
-        --output=messages.pot \
-        --from-code=utf-8 \
-        --msgid-bugs-address=translations@mindstellar.com
-
+find ./oc-admin/ ./oc-includes/osclass/ -type f -name '*.php' |
+  xargs xgettext \
+    --keyword=_m -k=_mn:1,2 \
+    --language=PHP \
+    --output-dir="$ROOT_DIR"/src/templates/ \
+    --output=messages.pot \
+    --from-code=utf-8 \
+    --msgid-bugs-address=translations@mindstellar.com
 echo 'messages.pot is created in /src/templates directory'
-
-#Create Fresh gettext pot file messages
+# clean up Osclass directory safely
+cd ..
+rm -rf Osclass
+# Now clone https://github.com/mindstellar/theme-bender repo and change directory to it
+git clone git@github.com:mindstellar/theme-bender.git
+cd theme-bender || exit
+#Create Fresh gettext pot file for theme-bender
 # shellcheck disable=SC2038
-find tmp/osclass/oc-content/themes/bender -type f -name '*.php' |
-    xargs xgettext \
-        --keyword=__ -k=_e -k=_m -k=_mn:1,2 \
-        --language=PHP \
-        --output-dir=src/templates \
-        --output=theme.pot \
-        --from-code=utf-8 \
-        --msgid-bugs-address=translations@mindstellar.com
-
+find . -type f -name '*.php' |
+  xargs xgettext \
+    --keyword=__ -k=_e -k=_m -k=_mn:1,2 \
+    --language=PHP \
+    --output-dir="$ROOT_DIR"/src/templates/ \
+    --output=theme.pot \
+    --from-code=utf-8 \
+    --msgid-bugs-address=translations@mindstellar.com
 echo 'theme.pot is created in /src/templates directory'
+# clean up theme-bender directory
 echo 'Cleaning temporary files'
-
-cp src/mail.json src/templates/mail.json
-# Remove temporary files
-rm -r ./tmp
-
+cd ..
+rm -rf theme-bender
+# Copy mail.json and locale.json to src/templates directory
+echo 'Copying mail.json and locale.json to /src/templates directory'
+cp "$ROOT_DIR"/src/locale.json "$ROOT_DIR"/src/templates/
+cp "$ROOT_DIR"/src/mail.json "$ROOT_DIR"/src/templates/
+# Clean up tmp directory
+echo 'Cleaning temporary files'
+cd ..
+rm -rf tmp
 echo 'Done'
